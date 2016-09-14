e3cdf16725e7f30836e2f339fdad973673829b0e
jcb23 <bencjohn@vt.edu>
2016-09-14 17:54:41 -0400

    Added Ben's MS1 directory

---
+++ milestones/one/Ben_Code/Team13_MS1/firmware/Team13_MS1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# MS1 configuration
+CND_ARTIFACT_DIR_MS1=dist/MS1/production
+CND_ARTIFACT_NAME_MS1=Team13_MS1.X.production.hex
+CND_ARTIFACT_PATH_MS1=dist/MS1/production/Team13_MS1.X.production.hex
+CND_PACKAGE_DIR_MS1=${CND_DISTDIR}/MS1/package
+CND_PACKAGE_NAME_MS1=team13ms1.x.tar
+CND_PACKAGE_PATH_MS1=${CND_DISTDIR}/MS1/package/team13ms1.x.tar
