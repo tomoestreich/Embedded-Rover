103f3598090cff91636c922600eb4d869c5faffd
root <root@raspberrypi.(none)>
2016-09-27 06:20:32 +0000

    Minor changes to Pi hosting config

--- control/comm/comm_ctrl.py
+++ control/comm/comm_ctrl.py
@@ -23,34 +23,48 @@ import socket
 #---------------------------------------------------------------------
 def main():
 	# create socket and address objects
-	sock_two = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serv_two = ('192.168.42.2', 2000)
+	sock_one = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#sock_two = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serv_one = ('192.168.42.2', 2001)
+	serv_two = ('192.168.42.2', 2002)
+	serv_three = ('192.168.42.2', 2003)
+	serv_four = ('192.168.42.2', 2004)
 	
 	# Bind the socket and print status -- listen until a connection is made
-	print "Starting socket %s:%s" % serv_two
-	sock_two.bind(serv_two)
-	sock_two.listen(1)
+	print "Starting socket %s:%s" % serv_one
+	sock_one.bind(serv_one)
+	sock_one.listen(1)
+	#print "Starting socket %s:%s" % serv_two
+	#sock_two.bind(serv_two)
+	#sock_two.listen(1)
 
 	# loop continuously waiting for information
 	while(1):
-		print "Waiting for a connection..."
-		conn, addr_two = sock_two.accept()
-		
+		print "Waiting for a connection(s)..."
+		conn_one, addr_one = sock_one.accept()
+		#conn_two, addr_two = sock_two.accept()
+
 		# Receive data from client
 		try:
-			print "Connection found: ", addr_two
+			print "Connection(s) found: ", addr_one
 			while True:
-				msg_two = conn.recv(16)
-				if msg_two and msg_two != "^":
-					print "Message Received: %s" % msg_two
-				elif msg_two == "^":
-					print "Connection closed by WiFly two"
+				msg_one = conn_one.recv(16)
+				if msg_one == "*HELLO*":
+					continue
+				elif msg_one and msg_one != "*CLOS*":
+					print "WiFly1 Message Received: %s" % msg_one
+					print "Sending Message to WiFly2..."
+					conn_one.sendall(msg_one[0])
+					print "Message sent."
+				elif msg_one == "*CLOS*":
+					print "Connection closed by WiFly one"
 					break
 				else:
-					print "Data transfer complete. Exiting..."
+					print "Data transfer complete. WiFlys Closing..."
 					break	
 		finally:
-			conn.close()
+			conn_one.close()
+			#conn_two.close()
 			break
 
 if __name__ == "__main__":
